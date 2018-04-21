9125af9d2cb821524727ce9d08a3ea3a76e64d68
zschneid85 <31906001+zschneid85@users.noreply.github.com>
2018-04-20 22:32:12 -0400

    Update htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,8 @@
-// Pull in required dependencies
-var path = require('path');
+var path = require("path");
 
-// Export HTML routes
 module.exports = function(app) {
-	// console.log('___ENTER htmlRoutes.js___');
 
-	// Home page
-app.get("/survey", function(req, res) {
+  app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
@@ -14,3 +10,5 @@ app.get("/survey", function(req, res) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
+
+};
