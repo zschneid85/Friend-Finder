ce8b3b19112e79d4bdae9693f55319a367c7cb55
zschneid85 <31906001+zschneid85@users.noreply.github.com>
2018-04-20 22:04:40 -0400

    Update htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,3 +15,11 @@ module.exports = function(app) {
 		res.sendFile(path.join(__dirname, '../public/survey.html'));
 	});
 };
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
